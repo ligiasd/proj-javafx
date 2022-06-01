8d25095f5a682724ce757d0ed258a7b7cb69705a
Ligiane <ligiane.alves.souza@gmail.com>
2022-06-01 18:48:21 -0300

    add method button on click

--- javaFX2/src/main/java/com/example/javafx2/HelloController.java
+++ javaFX2/src/main/java/com/example/javafx2/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.javafx2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button btTest;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onBtTestAction() {
+        System.out.println("Hello!");
     }
 }
